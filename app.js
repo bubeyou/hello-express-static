20328634240574c1d685b5199b06565c5806b03d
bubeyou <bewithyou11@gmail.com>
2022-05-16 23:13:04 +0700

    update log

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(8080, function(){
-    console.log("hello-express-static listening on port 8080...")
+    console.log("hello-express-static is lstening on port 8080...")
 });
 
